674ab29e339d83ec6628d5f291578ab45c137f77
QingchaoHu <huqch@126.com>
2024-03-22 18:01:17 +0800

    【新增】使用 Makefile 管理编译依赖

--- OpenGL01/brew.sh
+++ OpenGL01/brew.sh
@@ -2,7 +2,4 @@
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## install packages
-# brew install glfw glew assimp glm
-
-## compile
-g++ -std=c++17 -L/opt/homebrew/lib -I/opt/homebrew/include -lglfw -lglew -lglm -lassimp -framework OpenGL -o main *.cpp
+brew install glfw glew assimp glm
