a5f448b165411bd618df3dbfc5f768dad5396e6e
qingchaohu <qingchaohu@tencent.com>
2024-03-23 16:01:54 +0800

    【新增】使用默认库路径

--- OpenGL01/brew.sh
+++ OpenGL01/brew.sh
@@ -2,4 +2,5 @@
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## install packages
-brew install glfw glew assimp glm
+# brew install --debug-symbols --build-from-source glfw glew assimp glm # debug
+brew install glfw glew assimp glm # release
